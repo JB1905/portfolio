760ab962dd3d6969b6df175bec3c1ee81a4ddc89
Jakub Biesiada <biesiadajakub@icloud.com>
2018-12-13 18:39:27 +0100

    Update

--- src/components/View/index.js
+++ src/components/View/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './View.scss';
 
-const View = ({ className, children }) => (
+const View = ({ className = '', children }) => (
   <div className={`view ${className}`}>{children}</div>
 );
 
