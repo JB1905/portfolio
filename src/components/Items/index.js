65cfd5a5ea040d20030c27b756c6435367059ef8
Jakub Biesiada <biesiadajakub@icloud.com>
2019-03-29 23:56:31 +0100

    Prepare for v2

--- src/components/Items/index.js
+++
@@ -1,7 +0,0 @@
-export { default as MobileMenuItem } from './Menu/Mobile';
-export { default as DesktopMenuItem } from './Menu/Desktop';
-export { default as IconItem } from './Icon';
-
-export { default as TechnologyItem } from './Technology';
-export { default as ProjectItem } from './Project';
-export { default as ContactItem } from './Contact';
