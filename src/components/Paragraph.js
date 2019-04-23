4f5d40d2ad907f45ad693402af78a7f54de1ccda
Jakub Biesiada <biesiadajakub@icloud.com>
2019-04-24 00:16:02 +0200

    Update components

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Delay from 'react-delay';
+import PropTypes from 'prop-types';
 
-const Paragraph = ({ index, content }) => (
+const Paragraph = ({ content, index }) => (
   <Delay wait={(index + 1) * 240 + 520}>
     <p>{content}</p>
   </Delay>
 );
+
+Paragraph.propTypes = {
+  content: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired
+};
 
 export default Paragraph;
