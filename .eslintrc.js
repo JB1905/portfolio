1c6b8ab2f94258ed77b3d8a8c63ca34bcd434336
Jakub Biesiada <biesiadajakub@icloud.com>
2019-05-25 21:54:03 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
-{
-  'parser': 'babel-eslint',
-  'extends': [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/react'
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react"
   ],
-  'globals': {
-    'window': true,
-    'document': true
+  globals: {
+    window: true,
+    document: true
   },
-  'settings': {
-    'import/resolver': {
-      'node': {
-        'paths': ['./src'],
-        'extensions': ['.js']
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["./src"],
+        extensions: [".js"]
       }
     }
   },
-  'rules': {
-    'react/jsx-filename-extension': 0,
-    'import/prefer-default-export': 0
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "import/prefer-default-export": 0
   }
-}
+};
