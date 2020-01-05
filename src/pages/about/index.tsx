c77a4e64ae897c81c0fd58c21b4268e8101e4dae
Jakub Biesiada <biesiadajakub@icloud.com>
2020-01-05 15:06:48 +0100

    Update

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -48,7 +48,7 @@ const About = ({ data }: Props) => {
           <Delay wait={520}>
             <figure>
               <CSSTransition in appear timeout={1000} classNames="image">
-                {data.image && <Img fluid={data.image.childImageSharp.fluid} />}
+                <Img fluid={data.image.childImageSharp.fluid} />
               </CSSTransition>
             </figure>
           </Delay>
