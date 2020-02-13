b3710d43bd6c538d03df6530fcaa5399948b6c03
Jakub Biesiada <biesiadajakub@icloud.com>
2020-02-14 00:18:27 +0100

    Update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,23 +8,11 @@ const Home = () => (
   <Delay wait={280}>
     <div className="home__container">
       <div className="home__title">
-        <CSSTransition
-          in
-          appear
-          timeout={2000}
-          unmountOnExit
-          classNames="title"
-        >
+        <CSSTransition in appear timeout={2000} classNames="title">
           <h1>Jakub Biesiada</h1>
         </CSSTransition>
 
-        <CSSTransition
-          in
-          appear
-          timeout={2000}
-          unmountOnExit
-          classNames="subtitle"
-        >
+        <CSSTransition in appear timeout={2000} classNames="subtitle">
           <h2>JavaScript Developer</h2>
         </CSSTransition>
       </div>
