773253207a8d6f8577e9373732f655d5172cbf01
Jakub Biesiada <biesiadajakub@icloud.com>
2020-02-13 23:38:24 +0100

    Add delay

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,35 @@
 import React from "react";
+import Delay from "react-delay";
 import { CSSTransition } from "react-transition-group";
 
 import "./home.scss";
 
 const Home = () => (
-  <div className="home__container">
-    <div className="home__title">
-      <CSSTransition in appear timeout={2000} classNames="title">
-        <h1>Jakub Biesiada</h1>
-      </CSSTransition>
+  <Delay wait={280}>
+    <div className="home__container">
+      <div className="home__title">
+        <CSSTransition
+          in
+          appear
+          timeout={2000}
+          unmountOnExit
+          classNames="title"
+        >
+          <h1>Jakub Biesiada</h1>
+        </CSSTransition>
 
-      <CSSTransition in appear timeout={2000} classNames="subtitle">
-        <h2>JavaScript Developer</h2>
-      </CSSTransition>
+        <CSSTransition
+          in
+          appear
+          timeout={2000}
+          unmountOnExit
+          classNames="subtitle"
+        >
+          <h2>JavaScript Developer</h2>
+        </CSSTransition>
+      </div>
     </div>
-  </div>
+  </Delay>
 );
 
 export default Home;
