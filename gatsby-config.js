8127f654c38a2ca9b4fda04e1bc4f3175f2d0b63
Jakub Biesiada <biesiadajakub@icloud.com>
2020-03-23 20:19:07 +0100

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,9 +49,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-stylelint`,
-      options: {
-        files: [`src/**/*.scss`],
-      },
+      options: { files: [`src/**/*.scss`] },
     },
     `gatsby-transformer-json`,
     {
