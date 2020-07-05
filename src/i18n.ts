24763cdc18d6345a47569f6d7d4fbc6550583d03
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-05 23:24:28 +0200

    chore: add fallback lang

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.use(LanguageDetector).init({
+  fallbackLng: 'pl',
   resources: {
     pl: {
       translations: require('./locales/pl/translations.json'),
