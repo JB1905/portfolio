22ef6b3b7f6c51a162f01744486f95a9170d4645
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-12 18:10:56 +0200

    chore: update

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,22 @@
 import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.use(LanguageDetector).init({
-  fallbackLng: 'en',
-  whitelist: ['en', 'pl'],
-  resources: {
-    pl: {
-      translations: require('./locales/pl/translations.json'),
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    whitelist: ['en', 'pl'],
+    resources: {
+      pl: {
+        translations: require('./locales/pl/translations.json'),
+      },
+      en: {
+        translations: require('./locales/en/translations.json'),
+      },
     },
-    en: {
-      translations: require('./locales/en/translations.json'),
-    },
-  },
-  defaultNS: 'translations',
-});
+    defaultNS: 'translations',
+  });
 
 export default i18n;
