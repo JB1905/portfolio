3d6f1a27d2fba16da8b68e33bfd48ea4cdf27b5b
Jakub Biesiada <biesiadajakub@icloud.com>
2020-08-29 20:15:56 +0200

    Refactor (#155)

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,38 +1,30 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import SEO from '../components/seo';
-import Title from '../components/title';
-import Content from '../components/content';
+import Page from '../components/page';
 import { Contact as ContactCard } from '../components/cards';
 
 const Contact: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <article>
-      <SEO title={t('contact.title')} />
+    <Page title={t('contact.title')} className="contact">
+      <ContactCard
+        icon="envelope"
+        delay={520}
+        title={t('contact.email.name')}
+        permalink={t('contact.email.value')}
+        method="mailto"
+      />
 
-      <Title>{t('contact.title')}</Title>
-
-      <Content className="contact">
-        <ContactCard
-          icon="envelope"
-          delay={520}
-          title={t('contact.email.name')}
-          permalink={t('contact.email.value')}
-          method="mailto"
-        />
-
-        <ContactCard
-          icon="phone"
-          delay={760}
-          title={t('contact.phone.name')}
-          permalink={t('contact.phone.value')}
-          method="tel"
-        />
-      </Content>
-    </article>
+      <ContactCard
+        icon="phone"
+        delay={760}
+        title={t('contact.phone.name')}
+        permalink={t('contact.phone.value')}
+        method="tel"
+      />
+    </Page>
   );
 };
 
